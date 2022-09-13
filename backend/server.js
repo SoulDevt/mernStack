ba1143c13d6f9dfcf57873e2464ccc724989b4a5
SoulTheDevt <souleymane.gueye.pro@gmail.com>
2022-09-13 22:12:21 +0200

    Express Router & API Routes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 require('dotenv').config()
 const express = require('express');
+const workoutRoutes = require('./routes/workouts');
 
 //use express app
 const app = express();
 
-//routes
-app.get("/", (req,res) => {
-    res.json({mssg: "Welcome Oklmzer"});
+//middleware
+app.use((req,res,next) => {
+    console.log(req.path, req.method)
+    next() //important to put next, pour passer à la fonction suivante après le middleware
 })
+
+app.use(express.json())//allows to use req.body in POST and PATCH requests
+
+//routes
+app.use('/api/workouts', workoutRoutes) // allows to use routes from routes folder and exemple: /api/workouts/:id
+
+// app.get("/", (req,res) => {
+//     res.json({mssg: "Welcome Oklmzer"});
+// })
 
 //listen for request
 app.listen(process.env.PORT, () => {
